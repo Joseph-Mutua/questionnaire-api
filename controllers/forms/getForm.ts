919912f336fd157604703ebf31349fdd4666fbaa
Joseph Mutua <Mutuaj793@gmail.com>
2024-05-13 14:31:07 +0300

    Minor update

--- controllers/forms/getForm.ts
+++ controllers/forms/getForm.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 
 import { Router, Response } from "express";
-import { AuthRequest } from "../../middleware/auth";
+import { AuthRequest, authenticateUser } from "../../middleware/auth";
 import HttpError from "../../utils/httpError";
 import { pool } from "../../config/db";
 
@@ -9,20 +9,48 @@ import { fetchFormDetails } from "../../helpers/forms/formControllerHelpers";
 
 const router = Router();
 
-//Form owner route for fetching form
-router.get("/:id", async (req: AuthRequest, res: Response) => {
-  const form_id = parseInt(req.params.id);
-  if (!form_id) {
-    throw new HttpError("Invalid form ID provided.", 400);
-  }
+router.get(
+  "/:id",
+  authenticateUser,
+  async (req: AuthRequest, res: Response) => {
+    const form_id = parseInt(req.params.id);
+    if (!form_id) {
+      throw new HttpError("Invalid form ID provided.", 400);
+    }
 
-  const form_details = await fetchFormDetails(pool, form_id);
+    const user_id = req.user?.user_id;
+    if (!user_id) {
+      throw new HttpError("User must be logged in.", 403);
+    }
 
-  if (!form_details) {
-    throw new HttpError("Form not found.", 404);
+    const roleCheckQuery = `
+    SELECT r.name FROM form_user_roles fur
+    JOIN roles r ON fur.role_id = r.role_id
+    WHERE fur.form_id = $1 AND fur.user_id = $2;
+  `;
+    const roleResult = await pool.query<{ name: string }>(roleCheckQuery, [
+      form_id,
+      user_id,
+    ]);
+
+    if (
+      roleResult.rowCount === 0 ||
+      !roleResult.rows.some((row) =>
+        ["Owner", "Editor", "Viewer"].includes(row.name)
+      )
+    ) {
+      throw new HttpError(
+        "Unauthorized access. Only owners, editors, or viewers can access the form details.",
+        403
+      );
+    }
+
+    const form_details = await fetchFormDetails(pool, form_id);
+    if (!form_details) {
+      throw new HttpError("Form not found.", 404);
+    }
+    res.json(form_details);
   }
-  
-  res.json(form_details);
-});
+);
 
 export default router;
